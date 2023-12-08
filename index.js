e49769faa501f9fedecd311d28bf8e04a3b62a38
sulisak <sarlemonvannasing@gmail.com>
2023-12-08 11:19:59 +0700

    crud buy detail

--- index.js
+++ index.js
@@ -1,43 +1,137 @@
-require('dotenv').config()
-var express = require('express')
-var cors = require('cors')
-const mysql = require('mysql2');
-
+require("dotenv").config();
+var express = require("express");
+var cors = require("cors");
+const mysql = require("mysql2");
+// const bodyParser = require("body-parser");
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
 // create the connection to database
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
+  database: process.env.DB_DATABASE,
 });
 
-var app = express()
+var app = express();
 
-app.use(cors())
+app.use(cors());
+app.use(express.urlencoded());
+app.use(express.json());
 
-app.get('/helloworld', function (req, res, next) {
-  res.json({msg: 'helloworld'})
-})
+app.get("/helloworld", function (req, res, next) {
+  res.json({ msg: "helloworld" });
+});
 
-app.get('/users', function (req, res, next) {
+app.get("/users", function (req, res, next) {
+  // simple query
+  connection.query("SELECT * FROM users", function (err, results, fields) {
+    res.json(results);
+  });
+});
+app.get("/buy_details", function (req, res, next) {
   // simple query
   connection.query(
-    'SELECT * FROM users',
-    function(err, results, fields) {
-      res.json(results)
+    `SELECT b.id,p.name as product,b.qty as qty,u.name as username,u.email as useremail FROM product p 
+    join buy b on p.id = b.productId join users u on u.id=b.uId`,
+    function (err, results, fields) {
+      res.json(results);
     }
   );
-})
-app.get('/buy_details', function (req, res, next) {
-  // simple query
+});
+
+// create ================
+
+app.post("/buy_details", function (req, res) {
+  const { productId, qty, uId } = req.body;
+
+  // Create an object to store the data
+  const databuy = {
+    productId: productId,
+    qty: qty,
+    uId: uId,
+  };
+  // Perform the database insertion
   connection.query(
-    `SELECT p.name as product,b.qty as qty,u.name as username,u.email as useremail FROM product p join buy b on p.id = b.productId join users u on u.id=b.uId`,
-    function(err, results, fields) {
-      res.json(results)
+    "INSERT INTO buy SET ?",
+    databuy,
+    function (error, results, fields) {
+      console.log(databuy);
+      if (error) {
+        console.error("Can not into the database: " + error.stack);
+        res.status(500).json({ error: "Can not into the database" });
+        return;
+      }
+      // Successful insertion
+      res.status(200).json({ message: "inserted successfully " });
     }
   );
-})
+});
+// create ================
+// delete ==================
+app.delete("/buy_details/:id", function (req, res) {
+  const id = req.params.id;
+
+  // Perform the database deletion
+  connection.query(
+    "DELETE FROM buy WHERE id = ?",
+    id,
+    function (error, results, fields) {
+      if (error) {
+        console.error("Error deleting data from the database: " + error.stack);
+        res
+          .status(500)
+          .json({ error: "Error deleting data from the database" });
+        return;
+      }
+
+      // Check if any rows were affected
+      if (results.affectedRows === 0) {
+        res.status(404).json({ error: "Record need to delete not found in the database" });
+        return;
+      }
+      // Successful deletion
+      res.status(200).json({ message: "Data deleted successfully " });
+    }
+  );
+});
+// delete ==================
+// update ==================
+app.put("/buy_details/:id", function (req, res) {
+  const id = req.params.id;
+  const { productId, qty, uId } = req.body;
+
+  // Create an object to store the updated data
+  const updatedData = {
+    productId: productId,
+    qty: qty,
+    uId: uId,
+  };
+
+  // Perform the database update
+  connection.query(
+    "UPDATE buy SET ? WHERE id = ?",
+    [updatedData, id],
+    function (error, results, fields) {
+      if (error) {
+        console.error("Error updating data in the database: " + error.stack);
+        res.status(500).json({ error: "Error updating data in the database" });
+        return;
+      }
+
+      // Check if any rows were affected
+      if (results.affectedRows === 0) {
+        res.status(404).json({ error: "Data not found in the database" });
+        return;
+      }
+
+      // Successful update
+      res.status(200).json({ message: "Data updated successfully" });
+    }
+  );
+});
+// update ==================
 
 app.listen(4000, function () {
-  console.log('CORS-enabled web server listening on port 4000')
-})
+  console.log("CORS-enabled web server listening on port 4000");
+});
